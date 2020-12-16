0de5a5ae65af4a9fe7531b672748a058f9f37019
Adam Mills <adamm2243@gmail.com>
2020-12-16 15:28:05 -0500

    Create test_checks.py

---
+++ test_checks.py
@@ -0,0 +1,7 @@
+from group_project import Checks
+import pytest
+
+def test_checks():
+    ''' this will test all the checks inside the Checks class'''
+    c = Checks()
+    assert c.ones([1,1,2,3,4]) == 2
