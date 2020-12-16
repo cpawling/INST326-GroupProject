e4f8369904a501e82437c36256f9b8a547e98c61
Adam Mills <adamm2243@gmail.com>
2020-12-16 17:30:49 -0500

    Update test_checks.py

--- test_checks.py
+++ test_checks.py
@@ -6,3 +6,8 @@ def test_checks():
     c = Checks()
     assert c.ones([1,1,2,3,4]) == 2
     assert c.twos([1,2,2,2,3]) == 6
+    assert c.threes([1,2,3,3,3]) == 9
+    assert c.fours([4,4,4,4,1]) == 16
+    assert c.fives([1,5,5,5,1]) == 15
+    assert c.sixes([1,6,1,6,1]) == 12
+    
